c2c8e603014a4b11a75ed74b92b013ccd4306ac7
Alberto Dal Bianco <alberto.dalbianco@IE4-CP-MAC505.local>
2022-11-12 08:53:15 +0000

    Refactored platform config getter

--- utils/platform.ts
+++ utils/platform.ts
@@ -1,8 +1,12 @@
 import platformsColors from "../platforms.json"
 
 const currentPlatform = process.env.PLATFORM ? (platformsColors.find(platform => platform.name === process.env.PLATFORM) ?? throwError('The PLATFORM .env variable is not correct'))  : throwError('The PLATFORM .env variable is not set')
-const currentPlatformColors = currentPlatform?.colors
+
+const getPlatformConfig = (config: String) => {
+    return currentPlatform[config] ? currentPlatform[config] : throwError('The specified configuration wasn\'t found')
+}
 
 export default {
-    currentPlatformColors,
+    currentPlatformColors: getPlatformConfig('colors'),
+    getPlatformConfig,
 }
